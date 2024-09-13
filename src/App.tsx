24c07f0a8053a603ac1cf5331fb230a876f7e587
BrunoHenrique258 <bruno.oliveira258@fatec.sp.gov.br>
2024-09-13 20:53:09 -0300

    new file:   .env 	new file:   .gitignore 	new file:   README.md 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   public/index.html 	new file:   src/App.css 	new file:   src/App.test.tsx 	new file:   src/App.tsx 	new file:   src/index.cs 	new file:   src/index.tsx 	new file:   src/logo.svg 	new file:   src/react-app-env.d.ts 	new file:   src/reportWebVitals.ts 	new file:   src/setupTests.ts 	new file:   tsconfig.json

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="container">
+      <header className="header">
+        <h1>Bem-vindo ao NutriTech</h1>
+        <p>olá</p>
+      </header>
+      <main className="main-Content">
+        <button className="button">Clique aqui</button>
+      </main>
+      
+    </div>
+  );
+}
+
+
