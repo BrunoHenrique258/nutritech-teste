4db29a776976e3b4251cd5d3fbd180dd36096a8b
BrunoHenrique258 <bruno.oliveira258@fatec.sp.gov.br.com>
2024-09-16 22:10:14 -0300

    modified:   src/App.css 	modified:   src/App.tsx 	new file:   src/Background_Image_NutriTech.PNG 	new file:   src/logo.nutritech.png.png

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
 import './App.css';
 import "./"
+import logo from './logo.nutritech.png.png';
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet"></link>
 
 export default function App() {
   return (
-    <div className="container">
-      <header className="header">
-        <h1>Bem-vindo ao NutriTech</h1>
-        <p>olá</p>
-        <label>Nome</label>
-        <input />
-      </header>
-      <main className="main-Content">
-        <button className="button">Clique aqui</button>
-      </main>
-      
+    <div className="background">
+     <div className="container-right">
+        <img src={logo} alt="Logo NutriTech" className="logo" />
+        <h2>O MELHOR PARA A SUA SAÚDE</h2>
+      </div> 
+     <div className="container-left">
+         <h1>Login</h1>
+         <label className="email-label">Email:</label>
+         <input type="email" className="email-input" />
+         <label className="password-label">Senha:</label>
+         <input type="password" className="password-input" />
+       <main className="main-Content">
+         <button className="button">Clique aqui</button>
+       </main>
+     </div>
     </div>
   );
+
 }
 
 
+// * foi removido o header * //
